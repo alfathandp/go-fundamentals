84c62a4881be2064f071050255b6625556db54fb
Alfathan <alfathandp@gmail.com>
2022-11-16 11:44:53 +0700

    add slice

--- slice.go
+++ slice.go
@@ -1,2 +1,14 @@
 package main
 
+import "fmt"
+
+func main() {
+
+	// append & copy
+	var colors = []string{"red","blue","green"}
+	colors = append (colors,"yellow")
+	copied_colors := make([]string,100)
+
+	copy(copied_colors, colors)
+	fmt.Println(copied_colors)
+}
